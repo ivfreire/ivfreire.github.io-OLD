1ae901ee48c2afd6e9a3926229dc1d415eb33026
Ícaro Freire <icarovf@outlook.com>
2022-04-20 19:36:29 -0300

    my page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/*
-	Created by Ícaro Freire on October 5th, 2021.
-	São Paulo, Brazil
-	https://github.com/ivfreire/
-*/
-
 window.onload = function() {
-	$.get('https://icarofreire.com/?s=reddit', function(data) {
-		console.log(data);
-	});
+	
 }
